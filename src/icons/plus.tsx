ca32e04a1bd38e25c0795e3892f308eac48c2c14
Andre Simoes <andrersfcosta@gmail.com>
2021-03-18 01:31:34 -0300

    feature(input): make input component

---
+++ src/icons/plus.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const PlusIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="16"
+    height="16"
+    data-testid="PlusIcon"
+  >
+    <path
+      fill="#FA7268"
+      d="M16 7.238H8.762V0H7.238v7.238H0v1.524h7.238V16h1.524V8.762H16z"
+    ></path>
+  </svg>
+);
+
+export default PlusIcon;
