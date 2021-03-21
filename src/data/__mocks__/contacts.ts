3a0fc404e2792eea800c14a0950bf24055574de7
Andre Simoes <andrersfcosta@gmail.com>
2021-03-21 19:39:12 -0300

    feature(table): make filledlist component

---
+++ src/data/__mocks__/contacts.ts
@@ -0,0 +1,31 @@
+import { ContactType } from "../../components/list/filledList";
+
+export const OneContactMock: ContactType[] = [
+  {
+    name: "Alana",
+    email: "alana@mail.com",
+    phone: "(11) 98432-9263",
+    color: "#231",
+  },
+];
+
+export const ContactsMock: ContactType[] = [
+  {
+    name: "Bianca",
+    email: "bianca@mail.com",
+    phone: "(11) 98430-9263",
+    color: "#dfe",
+  },
+  {
+    name: "Alana",
+    email: "alana@mail.com",
+    phone: "(11) 98432-9263",
+    color: "#231",
+  },
+  {
+    name: "Carlos",
+    email: "carlos@mail.com",
+    phone: "(11) 98431-9263",
+    color: "#872",
+  },
+];
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -6,37 +6,91 @@ import Header from "../../components/header";
 import EmptyList from "../../components/list/emptyList";
 import { ModalContext } from "../../components/modal/modalContext";
 import FormInput from "../../components/formInput";
+import FilledList from "../../components/list/filledList";
 
 const Home = () => {
   const { handleModal } = useContext(ModalContext);
 
+  const aa = [
+    {
+      name: "Bianca",
+      email: "bianca@mail.com",
+      phone: "(11) 98432-9263",
+      color: "#dfe",
+    },
+    {
+      name: "Alana",
+      email: "alana@mail.com",
+      phone: "(11) 98432-9263",
+      color: "#231",
+    },
+  ];
+
   return (
     <div className={styles.home}>
-      <Header emptyList={true} />
-      <EmptyList
-        onClick={() =>
-          handleModal({
-            title: "Criar novo contato",
-            element: (
-              <React.Fragment>
-                <FormInput
-                  className={styles.home_inputSpace}
-                  placeholder=""
-                  label="Nome"
-                  id="name"
-                />
-                <FormInput
-                  className={styles.home_inputSpace}
-                  placeholder=""
-                  label="E-mail"
-                  id="email"
-                />
-                <FormInput placeholder="" label="Telefone" id="telefone" />
-              </React.Fragment>
-            ),
-          })
-        }
-      />
+      <Header emptyList={!aa} />
+      {!aa ? (
+        <EmptyList
+          onClick={() =>
+            handleModal({
+              title: "Criar novo contato",
+              element: (
+                <React.Fragment>
+                  <FormInput
+                    className={styles.home_inputSpace}
+                    placeholder=""
+                    label="Nome"
+                    id="name"
+                  />
+                  <FormInput
+                    className={styles.home_inputSpace}
+                    placeholder=""
+                    label="E-mail"
+                    id="email"
+                  />
+                  <FormInput placeholder="" label="Telefone" id="telefone" />
+                </React.Fragment>
+              ),
+            })
+          }
+        />
+      ) : (
+        <FilledList
+          onEditClick={() => {
+            handleModal({
+              title: "Editar contato",
+              element: (
+                <React.Fragment>
+                  <FormInput
+                    className={styles.home_inputSpace}
+                    placeholder=""
+                    label="Nome"
+                    id="name"
+                  />
+                  <FormInput
+                    className={styles.home_inputSpace}
+                    placeholder=""
+                    label="E-mail"
+                    id="email"
+                  />
+                  <FormInput placeholder="" label="Telefone" id="telefone" />
+                </React.Fragment>
+              ),
+            });
+          }}
+          onDeleteClick={() => {
+            handleModal({
+              title: "Excluir contato",
+              element: (
+                <React.Fragment>
+                  <span>Deseja realmente excluir o contato?</span>
+                </React.Fragment>
+              ),
+            });
+          }}
+          contacts={aa}
+        />
+      )}
     </div>
   );
 };
